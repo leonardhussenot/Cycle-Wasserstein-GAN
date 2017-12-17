4e4bfc5c18a408bdbd3e97ab1abba4d4ede5b3b5
Léonard Hussenot <leonard.hussenot@gmail.com>
2017-12-17 12:50:47 +0100

    Wasserstein loss functions - TODO

--- losses.py
+++ losses.py
@@ -69,3 +69,11 @@ def lsgan_loss_discriminator(prob_real_is_real, prob_fake_is_real):
     """
     return (tf.reduce_mean(tf.squared_difference(prob_real_is_real, 1)) +
             tf.reduce_mean(tf.squared_difference(prob_fake_is_real, 0))) * 0.5
+
+def wasserstein_loss_discriminator(prob_real_is_real, prob_fake_is_real):
+    # TODO: implement
+    pass
+
+def wasserstein_loss_generator(prob_real_is_real, prob_fake_is_real):
+    # TODO: implement
+    pass
